99720a271b025aed0a5c64e1427d29c73fec9a2f
Ismael Gomes <ismaelcosta1000@gmail.com>
2014-05-08 12:52:05 -0300

    initial commit

--- src/main/java/org/krloxz/samples/HelloServlet.java
+++ src/main/java/org/krloxz/samples/HelloServlet.java
@@ -9,7 +9,10 @@ import java.io.IOException;
 
 @WebServlet(value="/hello", name="helloServlet")
 public class HelloServlet extends GenericServlet {
-    @Override
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         res.getWriter().println("Hello world!!");
     }
